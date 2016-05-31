5bcedc6a8ef86a0699fca80f42e510860ec47d89
Jay-Robot <jota.ribeiro.silva@gmail.com>
2016-05-31 11:21:14 -0300

    adição de novas entidades

---
+++ src/Entidade/Colecao.java
@@ -0,0 +1,7 @@
+package Entidade;
+
+/**
+ * Created by jonat on 31/05/2016.
+ */
+public class Colecao {
+}
--- src/Entidade/Proximidade.java
+++
@@ -1,8 +0,0 @@
-package Entidade;
-
-/**
- * Created by jonat on 24/05/2016.
- */
-public enum Proximidade {
-    Íntimo, Próximo, Regular, Distante, Incompatível,
-}
